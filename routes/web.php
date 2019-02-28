5b6ac0f474542de58ae16c85ad9c51d756a1d67e
ACVer <2416582044@qq.com>
2019-02-28 21:21:14 +0800

    /usr/bin/push

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::group(['middleware'=>'cookie'], function (){
 //        Route::get('/user/found', 'UserController@getUserFound');
 
 //        Route::get('/user/laf', 'UserController@getUserPost');
-        Route::get('/user/laf', 'UserController@getUserLAF');
+        Route::get('/user/laf', 'UserController@getUserPost');
 
         Route::get('/user/info', 'UserController@getUserInfo');
         Route::post('/user/avatar', 'UserController@saveAvatar');
